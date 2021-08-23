237843c2a813b90b0b820a51357aa1c51329a91e
abdelrahman azmy gad <abdelrahmangad95@gmail.com>
2021-08-23 18:27:26 +0200

    broadcasting public channel

---
+++ app/Events/SendMessage.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+
+class SendMessage
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message=  $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('user-channel');
+    }
+
+    public function broadcastAs()
+    {
+        return 'UserEvent';
+    }
+    /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastWith()
+    {
+        return ['title'=>$this->message];
+    }
+
+
+}
